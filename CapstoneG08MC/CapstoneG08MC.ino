f7ebef557ffc2646cd59625856b0fd82cd8eb4eb
JaeMark <deleonj3@myumanitoba.ca>
2019-11-19 22:12:16 -0600

    Synchonized ADC
    • Imported Pedvide's ADC library to simplify and improve ADC reading.
    • Configured ADC0 and ADC1 for synchronized reading. ADC0 will measure voltage input and ADC1 will measure current input.
    • Added a function to transmitt data packet. This function uses the ArduinoJason external library to pack the read volatge and current values with the sample number. Time is also planned to be transmitted.

--- CapstoneG08MC/CapstoneG08MC.ino
+++ CapstoneG08MC/CapstoneG08MC.ino
@@ -1,24 +1,38 @@
 #include <XBee.h>
 #include <ArduinoJson.h>
+#include <ADC.h>
 
 // constant declarations
 #define BAUD_RATE 9600
 
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+Rx64Response rx64 = Rx64Response();
+
+ADC *adc = new ADC();
+
 // pin declarations
-const int voltPin = 0; // place holder
-const int currPin = 1; // place holder
+const int voltPin = A9; // place holder
+const int currPin = A3; // place holder
 const int rxPin = 2; // place holder
 const int txPin = 3; // placeholder
 const int sleepPin = 4; // placeholder
 
 // global variables
-float voltReading = 0;
-float currReading = 0;
+double voltReading = 0;
+double currReading = 0;
 long sampleNum = 0;
 
-XBee xbee = XBee();
+char* cmd;
+
+uint8_t option = 0;
+uint8_t data = 0;
 
 void setup() {
+  // configure pins
+  pinMode(voltPin, INPUT);
+  pinMode(currPin, INPUT);
+  pinMode(sleepPin, INPUT);
 
   // start serial
   Serial.begin(BAUD_RATE);
@@ -28,31 +42,61 @@ void setup() {
   Serial.flush();
   xbee.begin(Serial);
 
-  // configure pins
-  pinMode(voltPin, INPUT);
-  pinMode(currPin, INPUT);
-  pinMode(sleepPin, INPUT);
+  // configure ADC0
+  adc->setAveraging(0, ADC_0);
+  adc->setResolution(12, ADC_0); 
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED, ADC_0);
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_LOW_SPEED, ADC_0);
+  adc->setReference(ADC_REFERENCE::REF_3V3, ADC_0);
+
+  // configure ADC1
+  adc->setAveraging(0, ADC_1); 
+  adc->setResolution(12, ADC_1); 
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED, ADC_1);
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_LOW_SPEED, ADC_1);
+
+  adc->startSynchronizedContinuous(voltPin, currPin);
+
+  delay(100);
+
 }
+
+ADC::Sync_result reading;
 
 void loop() {
-  xbee.readPacket();
-  if(xbee.getResponse().isAvailable()) {
-    // read command from workstation
-  }
+  // get analog pin readings
+  reading = adc->analogSynchronizedRead(voltPin, currPin);
+  reading.result_adc0 = (double)reading.result_adc0;
+  reading.result_adc1 = (double)reading.result_adc1;
 
-  // send data
+  voltReading = (double)reading.result_adc0*3.3/adc->getMaxValue(ADC_0);
+  currReading = (double)reading.result_adc1;
+  
+  sendData();
 }
 
-void readSensors() {
-  voltReading = readVoltPin();
-  currReading = readCurrPin();
+void handleCommand (uint8_t data) {
+//  DeserializationError error = deserializeMsgPack(packet, data);
+
 }
 
-float readVoltPin() {
-  double voltage = 0;
-  return voltage;
+void sendData() {
+    StaticJsonDocument<200> data;
+    data["sampleNum"] = sampleNum;
+    data["time"] = "2019-11-15 16:07";
+    data["volt"] = voltReading;
+    data["curr"] = currReading;
+    sampleNum++;
+    serializeJson(data, Serial);
+    Serial.println();
+   // serializeJsonPretty(data, Serial);
 }
-float readCurrPin() {
-  double current = 0;
+
+double readVoltPin() {
+  double voltage = analogRead(voltPin); // fake data
+  return voltage * (3.3 / 4095.0);;
+}
+double readCurrPin() {
+  double current = 0.12234; // fake data
   return current;
 }
