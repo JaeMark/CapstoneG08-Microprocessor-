99bdf3fa9b23e6f1f8aee919751a43e371c12be5
JaeMark <deleonj3@myumanitoba.ca>
2019-12-12 22:33:07 -0600

    Data Receiver and Sender Update
    • Added initialization to Serial1. This will receive and send data from and to the base station XBee device.
    • Removed XBee library as it is not needed for this implementation.
    • Set sampling and conversion speed for ADC0 and ADC1 to max (VERY_HIGH_SPEED).
    • Added function called handleReceivedMessage() to handle the received message in Serial1. The data packet will is a json packet that contains a command and the sample start number.
    • Changed json data buffer from static to dynamic.
    • A start ('<') and delimeter ('>'), will be sent before and after the json packet. This will simplify data parsing in the receiver.

--- CapstoneG08MC/CapstoneG08MC.ino
+++ CapstoneG08MC/CapstoneG08MC.ino
@@ -1,21 +1,14 @@
-#include <XBee.h>
 #include <ArduinoJson.h>
 #include <ADC.h>
 
 // constant declarations
 #define BAUD_RATE 9600
 
-XBee xbee = XBee();
-XBeeResponse response = XBeeResponse();
-Rx64Response rx64 = Rx64Response();
-
 ADC *adc = new ADC();
 
 // pin declarations
 const int voltPin = A9; // place holder
 const int currPin = A3; // place holder
-const int rxPin = 2; // place holder
-const int txPin = 3; // placeholder
 const int sleepPin = 4; // placeholder
 
 // global variables
@@ -23,10 +16,11 @@ double voltReading = 0;
 double currReading = 0;
 long sampleNum = 0;
 
-char* cmd;
+// commands
+const char* START_COMMAND = "START";
+const char* NO_COMMAND = "NO_COMMAND";
 
-uint8_t option = 0;
-uint8_t data = 0;
+const char* cmd = "NO_COMMAND";
 
 void setup() {
   // configure pins
@@ -36,67 +30,82 @@ void setup() {
 
   // start serial
   Serial.begin(BAUD_RATE);
-  while (!Serial) {
-    // wait till initialization is complete
-  }
+  Serial1.begin(BAUD_RATE);
+
   Serial.flush();
-  xbee.begin(Serial);
+  Serial1.flush();
 
   // configure ADC0
   adc->setAveraging(0, ADC_0);
-  adc->setResolution(12, ADC_0); 
-  adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED, ADC_0);
-  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_LOW_SPEED, ADC_0);
+  adc->setResolution(13, ADC_0); 
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED, ADC_0);
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED, ADC_0);
   adc->setReference(ADC_REFERENCE::REF_3V3, ADC_0);
 
   // configure ADC1
   adc->setAveraging(0, ADC_1); 
-  adc->setResolution(12, ADC_1); 
-  adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_LOW_SPEED, ADC_1);
-  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_LOW_SPEED, ADC_1);
+  adc->setResolution(13, ADC_1); 
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::VERY_HIGH_SPEED, ADC_1);
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::VERY_HIGH_SPEED, ADC_1);
 
   adc->startSynchronizedContinuous(voltPin, currPin);
+  //adc->startSynchronizedContinuousDifferential(A10, A11, A12, A13);
 
-  delay(100);
-
+  delay(2000);
 }
 
 ADC::Sync_result reading;
 
 void loop() {
-  // get analog pin readings
-  reading = adc->analogSynchronizedRead(voltPin, currPin);
-  reading.result_adc0 = (double)reading.result_adc0;
-  reading.result_adc1 = (double)reading.result_adc1;
+  if(Serial1.available()){
+    if(strcmp(cmd, NO_COMMAND) == 0) {
+      handleReceivedMessage();
+    } 
+  }
+    
+  if(strcmp(cmd, START_COMMAND) == 0) {
+      if(strcmp(cmd, START_COMMAND) == 0) {
+        while(true) {
+        // get analog pin readings
+        reading = adc->analogSynchronizedRead(voltPin, currPin);
+      
+        //reading = adc->analogSynchronizedReadDifferential(A10, A11, A12, A13);
+       
+        reading.result_adc0 = (double)reading.result_adc0;
+        reading.result_adc1 = (double)reading.result_adc1;
+      
+        voltReading = (double)reading.result_adc0*3.3/adc->getMaxValue(ADC_0);
+        currReading = (double)reading.result_adc1/adc->getMaxValue(ADC_1);
+      
+        sendData();
+        }
+      }
+   }
 
-  voltReading = (double)reading.result_adc0*3.3/adc->getMaxValue(ADC_0);
-  currReading = (double)reading.result_adc1;
-  
-  sendData();
+   // sleep
 }
 
-void handleCommand (uint8_t data) {
-//  DeserializationError error = deserializeMsgPack(packet, data);
-
+void handleReceivedMessage() {
+   DynamicJsonDocument data(100);
+   deserializeJson(data, Serial1);
+   cmd = data["command"];
+   sampleNum = data["sampleNum"];
 }
 
 void sendData() {
-    StaticJsonDocument<200> data;
+    DynamicJsonDocument dataBuffer(100);
+    JsonObject data = dataBuffer.to<JsonObject>();
     data["sampleNum"] = sampleNum;
     data["time"] = "2019-11-15 16:07";
     data["volt"] = voltReading;
     data["curr"] = currReading;
     sampleNum++;
-    serializeJson(data, Serial);
-    Serial.println();
-   // serializeJsonPretty(data, Serial);
-}
 
-double readVoltPin() {
-  double voltage = analogRead(voltPin); // fake data
-  return voltage * (3.3 / 4095.0);;
-}
-double readCurrPin() {
-  double current = 0.12234; // fake data
-  return current;
+   Serial1.write('<');
+   serializeJson(data, Serial1);
+   Serial1.write('>');
+   
+   //serializeJson(data, Serial);
+   //Serial.println();
+
 }
