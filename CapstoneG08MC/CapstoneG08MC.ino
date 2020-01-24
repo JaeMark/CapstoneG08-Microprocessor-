9bf64a21ad73077ddbaa47233a5b7f33aae6a609
JaeMark <deleonj3@myumanitoba.ca>
2020-01-23 21:11:11 -0600

    Code Cleanup + Wakeup Handshake
    • Removed commented out codes that are not needed anymore.
    • Added handshaking with the workstation when the XBee wakes up. This will help the workstation determine when to send the start command for the next sample.

--- CapstoneG08MC/CapstoneG08MC.ino
+++ CapstoneG08MC/CapstoneG08MC.ino
@@ -18,6 +18,7 @@
 #define DEFAULT_COMMAND 0
 #define START_COMMAND 1
 #define SLEEP_COMMAND 2
+#define WAKE_UP_COMMAND 3
 
 // global variables
 int cmd;
@@ -109,7 +110,7 @@ void loop() {
       small_delay_t = received_data["smallDelay"];
       big_delay_t = received_data["bigDelay"];
       
-      handshake();
+      startHandshake();
 
       unsigned long timeMicro;
       for (int isample = 0; isample < sampleNum; isample++) {
@@ -133,12 +134,15 @@ void loop() {
       pinMode(switchPin, OUTPUT);
       digitalWrite(switchPin, LOW);
 
+      // parse the rest of the received json object
+      sleep_time = received_data["sleepTime"];
+
+      
       timeStop = millis();
       long sleepDur = sleep_time - (timeStop - timeStart);
       if (sleepDur < 0) {
         sleepDur = 0;
       }
-      //delay(10000);
       delay(sleepDur);
 
       // wake up
@@ -147,19 +151,30 @@ void loop() {
       pinMode(switchPin, INPUT);
       digitalWrite(switchPin, HIGH);
 
+      wakeUpHandshake();
+      
       cmd = DEFAULT_COMMAND;
       break;
   }
 }
 
-void handshake() {
+void startHandshake() {
   DynamicJsonDocument dataBuffer(50);
   JsonObject data_to_send = dataBuffer.to<JsonObject>();
 
   data_to_send["command"] = START_COMMAND;
   data_to_send["sampleNum"] = sampleNum;
   serializeJson(data_to_send, Serial1);
-  delay(big_delay_t);
+  delay(small_delay_t);
+}
+
+void wakeUpHandshake() {
+  DynamicJsonDocument dataBuffer(50);
+  JsonObject data_to_send = dataBuffer.to<JsonObject>();
+
+  data_to_send["command"] = WAKE_UP_COMMAND;
+  serializeJson(data_to_send, Serial1);
+  delay(small_delay_t);
 }
 
 void sendReadings() {
@@ -167,19 +182,9 @@ void sendReadings() {
   JsonObject data = dataBuffer.to<JsonObject>();
 
   for (int isample = 1; isample < sampleNum + 1; isample++) {
-    //data["sampleNum"] = sampleNum++;
-    //data["time"] = "2019-11-15 16:07";
     data["volt"] = (double)voltReadings[isample - 1];
     data["curr"] = (double)currReadings[isample - 1];
     data["micros"] = (unsigned long)timeMicros[isample - 1];
-
-    //Serial.print(isample);
-    //Serial.print(",");
-    //Serial.print(voltReading, DEC);
-    //Serial.print(",");
-    //Serial.println(currReadings[isample], DEC);
-    //Serial.print(",");
-    //Serial.println(time, DEC);
 
     serializeJson(data, Serial1);
     if (isample % trans_delim == 0) {
@@ -190,91 +195,3 @@ void sendReadings() {
 
   }
 }
-
-// MISC.
-//      long isample = 0;
-//
-//      time = millis();
-//      readingADC0 = adc->analogRead(voltPin, ADC_0);
-//      readingADC1 = adc->analogReadDifferential(A10,A11, ADC_1);
-//      voltReading = (double)readingADC0*3.3/adc->getMaxValue(ADC_0);
-//      currReading = RESISTOR*(double)readingADC1*3.3/adc->getMaxValue(ADC_1);
-//
-//      double lastReadingMin = (double)voltReading*(1.0-TOLERANCE);
-//      double lastReadingMax = (double)voltReading*(1.0+TOLERANCE);
-//
-//      isample++;
-//      while((isample != NUM_SAMPLE_MAX || isample > NUM_SAMPLE_MAX) && voltReading > lastReadingMin && voltReading < lastReadingMax) {
-//        //for(int isample = 0; isample < NUM_SAMPLE_MIN; isample++) {
-//
-//          sendData();
-//      //for(long isample = 0; isample < NUM_SAMPLES; isample++) {
-//        // get analog pin readings
-//        //reading = adc->analogSynchronizedRead(voltPin, currPin);
-//
-//        //reading = adc->analogSynchronizedReadDifferential(A10, A11, A12, A13);
-//
-//        //reading.result_adc0 = (double)reading.result_adc0;
-//        //reading.result_adc1 = (double)reading.result_adc1;
-//
-//        //voltReading = (double)reading.result_adc0*3.3/adc->getMaxValue(ADC_0);
-//        //currReading = RESISTOR*(double)reading.result_adc1*3.3/adc->getMaxValue(ADC_1);
-//          if(isample >= NUM_SAMPLE_MAX) {
-//            break;
-//          }
-//          time = millis();
-//          // get analog pin readings
-//          readingADC0 = adc->analogRead(voltPin, ADC_0);
-//          readingADC1 = adc->analogReadDifferential(A10,A11, ADC_1);
-//          voltReading = (double)readingADC0*3.3/adc->getMaxValue(ADC_0);
-//          currReading = RESISTOR*(double)readingADC1*3.3/adc->getMaxValue(ADC_1);
-//
-//          isample++;
-//      }
-
-//  if(Serial1.available()){
-//    if(strcmp(cmd, NO_COMMAND) == 0) {
-//      handleReceivedMessage();
-//    }
-//  }
-//
-//  if(strcmp(cmd, START_COMMAND) == 0) {
-//      long timeStart = millis();
-//      pinMode(switchPin, INPUT);
-//      digitalWrite(switchPin, HIGH);
-//
-//      unsigned long timeMicro;
-//      for(long isample = 0; isample < NUM_SAMPLES; isample++) {
-//          timeMicro = micros();
-//          // get analog pin readings
-//          //reading = adc->readSynchronizedContinuous();
-//          readingADC0 = adc->analogRead(voltPin, ADC_0);
-//          readingADC1 = adc->analogRead(currPin, ADC_1);
-//          //voltReading = (double)readingADC0*3.3/adc->getMaxValue(ADC_0);
-//          //currReading = (double)readingADC1*3.3/adc->getMaxValue(ADC_1)/(double)RESISTOR;
-//          //voltReadings[isample] = (double)reading.result_adc0*3.3/adc->getMaxValue(ADC_0);
-//          //currReadings[isample] = (double)reading.result_adc1*3.3/adc->getMaxValue(ADC_1);
-//          voltReadings[isample] = (double)readingADC0*3.3/adc->getMaxValue(ADC_0);
-//          currReadings[isample] = (double)readingADC1*3.3/adc->getMaxValue(ADC_1);
-//          timeMicros[isample] = timeMicro;
-//      }
-//      sendReadings();
-//
-//      // sleep
-//      pinMode(sleepPin, INPUT);
-//      digitalWrite(sleepPin, HIGH);
-//      pinMode(switchPin, OUTPUT);
-//      digitalWrite(switchPin, LOW);
-//
-//      long timeStop = millis();
-//      long sleepDur = SLEEP_TIME - (timeStop - timeStart);
-//      if(sleepDur < 0) {
-//        sleepDur = 0;
-//      }
-//      //delay(10000);
-//      delay(sleepDur);
-//
-//      // wake up
-//      pinMode(sleepPin, OUTPUT);
-//      digitalWrite(sleepPin, LOW);
-//   }
